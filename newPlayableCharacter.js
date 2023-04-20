18f767854ffe306a336d128aa1d3032f51b113df
natuthe1634 <Princenatu22@gmail.com>
2023-04-19 18:34:13 -0700

    finished

--- newPlayableCharacter.js
+++ newPlayableCharacter.js
@@ -1,28 +1,74 @@
-function newPlayableCharacter(x, y) {
-    const element = newImage('assets/green-character/static.gif')
+function newNonPlayableCharacter(x, y) {
+    let element = newImage("assets/red-character/static.gif");
     element.style.zIndex = 1;
-
-    function handleDirectionChange(direction) {
-        if (direction === null) {
-            element.src = `assets/green-character/static.gif`
-        }
-        if (direction === 'west') {
-            element.src = `assets/green-character/west.gif`
-        }
-        if (direction === 'north') {
-            element.src = `assets/green-character/north.gif`
-        }
-        if (direction === 'east') {
-            element.src = `assets/green-character/east.gif`
-        }
-        if (direction === 'south') {
-            element.src = `assets/green-character/south.gif`
-        }
+  
+    let direction = null;
+  
+    function moveCharacter() {
+      if (direction === "west") {
+        x -= 1;
+      }
+      if (direction === "north") {
+        y += 1;
+      }
+      if (direction === "east") {
+        x += 1;
+      }
+      if (direction === "south") {
+        y -= 1;
+      }
+      element.style.left = x + "px";
+      element.style.bottom = y + "px";
     }
-
-    move(element).withArrowKeys(x, y, handleDirectionChange)
-
+  
+    setInterval(moveCharacter, 1);
+  
+    async function walkEast(time) {
+      direction = "east";
+      element.src = `./assets/red-character/east.gif`;
+      await sleep(time);
+      stop();
+    }
+  
+    async function walkNorth(time) {
+      direction = "north";
+      element.src = `./assets/red-character/north.gif`;
+      await sleep(time);
+      stop();
+    }
+  
+    async function walkWest(time) {
+      direction = "west";
+      element.src = `./assets/red-character/west.gif`;
+      await sleep(time);
+      stop();
+    }
+  
+    async function walkSouth(time) {
+      direction = "south";
+      element.src = `./assets/red-character/south.gif`;
+      await sleep(time);
+      stop();
+    }
+  
+    function stop() {
+      direction = null;
+      element.src = `./assets/red-character/static.gif`;
+    }
+  
     return {
-        element: element
-    }
-}
+      element: element,
+      walkWest: walkWest,
+      walkNorth: walkNorth,
+      walkEast: walkEast,
+      walkSouth: walkSouth,
+      stop: stop,
+    };
+  }
+  
+  function sleep(time) {
+    return new Promise(resolve => {
+      setTimeout(resolve, time);
+    });
+  }
+  
